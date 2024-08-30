7c1f0227ed047b718b49dc19f8cb29be8d1941e8
imad273 <abbadimad0123@gmail.com>
2024-08-30 21:27:02 +0100

    feat: visitors devcices chart

--- admin_panel/app/dashboard/analytics/page.tsx
+++ admin_panel/app/dashboard/analytics/page.tsx
@@ -4,9 +4,10 @@ import { getUsersData } from './activeUsers'
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
 import VisitorChart from '@/components/charts/visitorChart'
 import AnalyticsSkeleton from '@/components/loading/analyticsSkeleton'
-import { TriangleAlert } from 'lucide-react'
+import { Banknote, CreditCard, TriangleAlert } from 'lucide-react'
 import { getDevicesData } from './devices'
 import DevicesPieChart from '@/components/charts/devicesPieChart'
+import supabase from '@/lib/supabaseClient'
 
 type visitorsProps = {
   date: string | null | undefined
@@ -28,6 +29,8 @@ const page = () => {
   ]);
 
   const [devicesData, setDevicesData] = useState<devicesProps[] | undefined>([]);
+  const [totalOrders, setTotalOrders] = useState<number>(0);
+  const [totalRevenue, setTotalRevenue] = useState<number>(0);
 
   function formatDate(analyticsDate: string) {
     const month = analyticsDate.substring(4, 6);
@@ -64,7 +67,6 @@ const page = () => {
         };
       });
 
-      console.log(devicesData);
       setDevicesData(devicesData);
 
       setVisitorsData(arr);
@@ -75,8 +77,38 @@ const page = () => {
   }, [])
 
   useEffect(() => {
-    console.log(visitorsData);
-  }, [visitorsData])
+    const fetchOrdersSum = async () => {
+      const { data, count } = await supabase
+        .from('orders')
+        .select('created_at, total_amount, order_status', { count: 'exact' });
+
+      count && setTotalOrders(count);
+      let sum = 0;
+      data?.map(order => (
+        order.order_status === "paid" ? sum += order.total_amount : sum += 0
+      ))
+
+      setTotalRevenue(sum);
+    }
+
+    fetchOrdersSum();
+  }, [])
+
+  function getPercentage(deviceName: string) {
+
+    if (devicesData && devicesData.length > 0) {
+      // Calculate the total sum
+      const totalSum = devicesData?.reduce((acc, item) => acc + item.value, 0);
+
+      // Find the device item
+      const device = devicesData?.find(item => item.name === deviceName);
+
+      // Return the percentage if device is found
+      if (device) {
+        return ((device.value / totalSum) * 100).toFixed(2) + '%';
+      }
+    }
+  }
 
   return (
     <section>
@@ -87,64 +119,90 @@ const page = () => {
       {loading ?
         <AnalyticsSkeleton />
         :
-        <div className='grid grid-cols-2 gap-5 my-5'>
-          <div>
+        <div>
+          <div className='grid grid-cols-2 gap-5'>
             <Card>
-              <CardHeader>
-                <CardTitle>Visitors</CardTitle>
-                <CardDescription>
-                  Showing total visitors for the last month
-                </CardDescription>
+              <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+                <CardTitle className="text-sm font-medium">Sales</CardTitle>
+
+                <CreditCard className="w-5 h-5 text-muted-foreground" />
               </CardHeader>
-              {visitorsData.length === 1 ?
-                <CardContent>
-                  <div className='flex flex-col items-center justify-center gap-3 h-[300px]'>
-                    <TriangleAlert size={26} />
-                    <p>No Data Available</p>
-                  </div>
-                </CardContent>
-                :
-                <CardContent className='p-2'>
-                  <VisitorChart data={visitorsData} />
-                </CardContent>
-              }
+              <CardContent>
+                <div className="text-2xl font-bold">{totalOrders}</div>
+              </CardContent>
+            </Card>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between pb-2 space-y-0">
+                <CardTitle className="text-sm font-medium">
+                  Total Revenue
+                </CardTitle>
+
+                <Banknote className="w-5 h-5 text-muted-foreground" />
+              </CardHeader>
+              <CardContent>
+                <div className="text-2xl font-bold">${totalRevenue}</div>
+              </CardContent>
             </Card>
           </div>
-
-          <div>
-            <Card>
-              <CardHeader>
-                <CardTitle>Devices</CardTitle>
-                <CardDescription>
-                  Showing visitors devices for the last month
-                </CardDescription>
-              </CardHeader>
-              {visitorsData.length === 1 ?
-                <CardContent>
-                  <div className='flex flex-col items-center justify-center gap-3 h-[300px]'>
-                    <TriangleAlert size={26} />
-                    <p>No Data Available</p>
-                  </div>
-                </CardContent>
-                :
-                <CardContent className='p-2'>
-                  <DevicesPieChart data={devicesData} />
-
-                  <CardFooter>
-                    <div>
-                      <div className='flex items-center gap-3'>
-                        <div className='w-4 h-4 rounded-md bg-[#F56B4A]'></div>
-                        Mobile
-                      </div>
-                      <div className='flex items-center gap-3'>
-                        <div className='w-4 h-4 rounded-md bg-[#F9CC5B]'></div>
-                        Desktop
-                      </div>
+          <div className='grid grid-cols-2 gap-5 my-5'>
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Visitors - {visitorsData?.reduce((acc, item) => acc + item.activeUsers, 0)}</CardTitle>
+                  <CardDescription>
+                    Showing total visitors for the last month
+                  </CardDescription>
+                </CardHeader>
+                {visitorsData.length === 1 ?
+                  <CardContent>
+                    <div className='flex flex-col items-center justify-center gap-3 h-[300px]'>
+                      <TriangleAlert size={26} />
+                      <p>No Data Available</p>
                     </div>
-                  </CardFooter>
-                </CardContent>
-              }
-            </Card>
+                  </CardContent>
+                  :
+                  <CardContent className='p-2'>
+                    <VisitorChart data={visitorsData} />
+                  </CardContent>
+                }
+              </Card>
+            </div>
+
+            <div>
+              <Card>
+                <CardHeader>
+                  <CardTitle>Devices</CardTitle>
+                  <CardDescription>
+                    Showing visitors devices for the last month
+                  </CardDescription>
+                </CardHeader>
+                {visitorsData.length === 1 ?
+                  <CardContent>
+                    <div className='flex flex-col items-center justify-center gap-3 h-[300px]'>
+                      <TriangleAlert size={26} />
+                      <p>No Data Available</p>
+                    </div>
+                  </CardContent>
+                  :
+                  <CardContent className='p-2'>
+                    <DevicesPieChart data={devicesData} />
+
+                    <CardFooter>
+                      <div>
+                        <div className='flex items-center gap-3'>
+                          <div className='w-4 h-4 rounded-md bg-[#F56B4A]'></div>
+                          Mobile - {getPercentage("mobile")}
+                        </div>
+                        <div className='flex items-center gap-3'>
+                          <div className='w-4 h-4 rounded-md bg-[#F9CC5B]'></div>
+                          Desktop - {getPercentage("desktop")}
+                        </div>
+                      </div>
+                    </CardFooter>
+                  </CardContent>
+                }
+              </Card>
+            </div>
           </div>
         </div>
       }
